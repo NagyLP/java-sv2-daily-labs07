74dc1e8ead4b0181d5e9ac6a23567a3dd9a62094
NagyLP <nagy.peter@pr.hu>
2021-11-22 15:41:58 +0100

    day01: Human (init)

--- src/main/java/day01/Human.java
+++ src/main/java/day01/Human.java
@@ -1,5 +1,15 @@
 package day01;
 
 public class Human {
-    
+
+    private String name;
+    private int yearOfBirth;
+
+    public Human(String firstname, String lastname, int yearOfBirth) {
+        this.name = firstname+ " " +lastname;
+        this.yearOfBirth = yearOfBirth;
+        if (yearOfBirth>120) {
+            throw new IllegalArgumentException("120 évesnél idősebb:" +yearOfBirth+ " éves, ez hiba.");
+        }
+    }
 }
