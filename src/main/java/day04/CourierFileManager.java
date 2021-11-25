cafe3f2ff32cdad085f1697b959ef059e107415a
NagyLP <nagy.peter@pr.hu>
2021-11-25 16:45:30 +0100

    day04: Ride (init)

---
+++ src/main/java/day04/CourierFileManager.java
@@ -0,0 +1,26 @@
+package day04;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourierFileManager {
+
+    public Courier createCourierByFile(Path path) {
+
+        try {
+            return createCourier(Files.readAllLines(path));
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file.", ioe);
+        }
+    }
+
+    private Courier createCourier(List<String> lines) {
+        Courier courier = new Courier();
+        for (String item : lines) {
+            String[] parts = item.split(" ");
+        }
+    }
+}
